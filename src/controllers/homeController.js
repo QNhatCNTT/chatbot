df734021786f42924c0c6b220e3749135c3c2302
QNhatCNTT <43816037+QNhatCNTT@users.noreply.github.com>
2021-06-03 21:20:05 +0700

    setup get started button

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import request from "request";
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
 };
@@ -142,8 +144,34 @@ function callSendAPI(sender_psid, response) {
     });
 }
 
+let setupProfile = async (req, res) => {
+    //call profile facebook api
+    // Construct the message body
+    let request_body = {
+        "get_started": { "payload": "GET_STARTED" },
+        "whitelisted_domains": ["https://demo-bot-chat.herokuapp.com/"]
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/v10.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        console.log(body);
+        if (!err) {
+            console.log('setup profile succeeds!')
+        } else {
+            console.error("Unable to setup profile:" + err);
+        }
+    });
+
+    return res.send("Setup profile succeed!");
+};
 module.exports = {
     getHomePage: getHomePage,
     postWebhook: postWebhook,
     getWebhook: getWebhook,
+    setupProfile: setupProfile,
 }
