dad496b86c03985d3f13903ef27c03b2e832e818
QNhatCNTT <43816037+QNhatCNTT@users.noreply.github.com>
2021-06-04 10:10:24 +0700

    update 5

--- src/service/chatbotService.js
+++ src/service/chatbotService.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import { response } from "express";
 import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
@@ -26,11 +27,36 @@ let callSendAPI = (sender_psid, response) => {
         }
     });
 };
+
+let getUserName = async (sender_psid) => {
+    // Construct the message body
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${serder_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log(body);
+        if (!err) {
+            response = JSON.parse(res);
+            /* "first_name": "Peter",
+            "last_name": "Chang", */
+            username = `${response.first_name} ${response.last_name}`;
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    return username;
+};
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let response = { "text": "Xin chào mừng bạn đến với ABC Study Online" }
+            let username = await getUserName(sender_psid);
+            let response = { "text": `Xin chào mừng ${username} đến với ABC Study Online` }
             await callSendAPI(sender_psid, response);
+            resolve('done');
         } catch (e) {
             reject(e);
         }
