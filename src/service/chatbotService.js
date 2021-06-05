cc68e5e8fd54781637efab9e1a03debd45de5815
QNhatCNTT <43816037+QNhatCNTT@users.noreply.github.com>
2021-06-05 10:44:09 +0700

    astasdf

--- src/service/chatbotService.js
+++ src/service/chatbotService.js
@@ -19,7 +19,7 @@ const IMAGE_MOBILE_KOTLIN = 'http://bit.ly/bot_kotlin';
 const IMAGE_MOBILE_SWIFT = 'http://bit.ly/bot_swift';
 
 
-let callSendAPI = (sender_psid, response) => {
+let callSendAPI = async (sender_psid, response) => {
     // Construct the message body
     let request_body = {
         "recipient": {
@@ -29,6 +29,8 @@ let callSendAPI = (sender_psid, response) => {
     }
 
     // Send the HTTP request to the Messenger Platform
+    await sendTypingon(sender_psid);
+    await MarkMessage(sender_psid);
     request({
         "uri": "https://graph.facebook.com/v10.0/me/messages",
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
@@ -39,6 +41,54 @@ let callSendAPI = (sender_psid, response) => {
             console.log('message sent!')
         } else {
             console.error("Unable to send message:" + err);
+        }
+    });
+};
+
+let sendTypingon = (sender_psid) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "sender_action": "typing_on"
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v10.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('sendTypingon sent!')
+        } else {
+            console.error("Unable to send sendTypingon:" + err);
+        }
+    });
+};
+
+let MarkMessage = (sender_psid) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "sender_action": "mark_seen"
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v10.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('sendTypingon sent!')
+        } else {
+            console.error("Unable to send sendTypingon:" + err);
         }
     });
 };
